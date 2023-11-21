60d33bf1fdfdf073111aa320cf0904673b3c1acd
abduljalal14 <abdoeljalalexotic@gmail.com>
2023-11-21 18:01:02 +0700

    Store Product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
+
 
 class ProductController extends Controller
 {
@@ -16,5 +18,36 @@ class ProductController extends Controller
 
         //return collection of posts as a resource
         return new ProductResource(true, 'List Data Posts', $products);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'name'     => 'required',
+            'desc'   => 'required',
+            'price'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/products-img', $image->hashName());
+
+        //create post
+        $product = Product::create([
+            'image'     => $image->hashName(),
+            'name'     => $request->name,
+            'desc'   => $request->desc,
+            'price'   => $request->price,
+        ]);
+
+        //return response
+        return new ProductResource(true, 'Data Produk Berhasil Ditambahkan!', $product);
     }
 }
