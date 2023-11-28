aead2f6be04c7d768b278d47135fe1b473147918
abduljalal14 <abdoeljalalexotic@gmail.com>
2023-11-28 11:12:54 +0700

    Category CRUD

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
+use Illuminate\Support\Facades\Validator;
 
 
 class CategoryController extends Controller
@@ -19,4 +20,57 @@ class CategoryController extends Controller
         return new CategoryResource(true, 'List Data Categories', $categories);
     }
 
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        
+        //create post
+        $category = Category::create([
+            'name'     => $request->name,
+        ]);
+
+        //return response
+        return new CategoryResource(true, 'Data Kategori Berhasil Ditambahkan!', $category);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find prdouct by ID
+        $category = Category::find($id);
+        
+        $category->update([
+                'name'     => $request->name,
+            ]);
+
+               //return response
+        return new CategoryResource(true, 'Data Kategori Berhasil Diubah!', $category);
+    }
+    public function destroy($id)
+    {
+        //find product by ID
+        $category = Category::find($id);
+        //delete product
+        $category->delete();
+        //return response
+        return new CategoryResource(true, 'Data Kategori Berhasil Dihapus!', null);
+    }
 }
